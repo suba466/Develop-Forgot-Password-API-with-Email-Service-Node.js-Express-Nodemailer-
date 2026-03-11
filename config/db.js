226a99e5f54ce13a9f6bedd00c4edc4e935c777e
suba466 <suba466@example.com>
2026-03-11 12:16:52 +0530

    Add Forgot Password API files

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: "postgres"
+    }
+);
+
+sequelize.authenticate().then(() => {
+    console.log("Database connected successfully");
+})
+    .catch((err) => {
+        console.log("Error connecting database:", err);
+    });
+
+module.exports = sequelize;
---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  forgotPassword,
+  resetPassword
+} = require("../controllers/authController");
+
+router.post("/forgot-password", forgotPassword);
+
+router.post("/reset-password", resetPassword);
+
+module.exports = router;
